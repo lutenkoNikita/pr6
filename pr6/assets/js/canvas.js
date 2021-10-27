a2a2890dfd8ac2cd84e12b2c4c08e8cc4c6deb4b
lutenkoNikita <90026027+lutenkoNikita@users.noreply.github.com>
2021-10-27 19:47:47 +0300

    Add files via upload

---
+++ pr6/assets/js/canvas.js
@@ -0,0 +1,137 @@
+﻿function canvas(selector, options){
+   const canvas = document.querySelector(selector);
+   canvas.classList.add('canvas')
+   canvas.setAttribute('width', `${options.width || 400}px`)
+   canvas.setAttribute('height', `${options.height || 300}px`)
+
+
+   // отримання контексту для малювання
+   const context = canvas.getContext('2d')
+   context.font = "22px Verdana"
+  // отримуємо координати canvas відносно viewport
+   const rect = canvas.getBoundingClientRect();
+let isPaint = false // чи активно малювання
+let points = [] //масив з точками
+
+	// об’являємо функцію додавання точок в масив
+const addPoint = (x, y, dragging) => {
+   // преобразуємо координати події кліка миші відносно canvas
+   points.push({
+       x: (x - rect.left),
+       y: (y - rect.top),
+       dragging: dragging
+   })
+}
+
+	 // головна функція для малювання
+   const redraw = () => {
+   //очищуємо  canvas
+   context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+   context.strokeStyle = options.strokeColor;
+   context.lineJoin = "round";
+   context.lineWidth = options.strokeWidth;
+   let prevPoint = null;
+   for (let point of points){
+       context.beginPath();
+       if (point.dragging && prevPoint){
+           context.moveTo(prevPoint.x, prevPoint.y)
+       } else {
+           context.moveTo(point.x - 1, point.y);
+       }
+       context.lineTo(point.x, point.y)
+       context.closePath()
+       context.stroke();
+       prevPoint = point;
+   }
+}
+
+	 // функції обробники подій миші
+const mouseDown = event => {
+   isPaint = true
+   addPoint(event.pageX, event.pageY);
+   redraw();
+}
+
+const mouseMove = event => {
+   if(isPaint){
+       addPoint(event.pageX, event.pageY, true);
+       redraw();
+   }
+}
+
+// додаємо обробку подій
+canvas.addEventListener('mousemove', mouseMove)
+canvas.addEventListener('mousedown', mouseDown)
+canvas.addEventListener('mouseup',() => {
+   isPaint = false;
+});
+canvas.addEventListener('mouseleave',() => {
+   isPaint = false;
+});
+
+// TOOLBAR
+const toolBar = document.getElementById('toolbar')
+// clear button
+const clearBtn = document.createElement('button')
+clearBtn.classList.add('btn')
+clearBtn.textContent = 'Clear'
+clearBtn.addEventListener('click', () => {
+   // points.clearRect 
+   context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+   points = [] 
+})
+toolBar.insertAdjacentElement('afterbegin', clearBtn)
+
+const saveBtn = document.getElementById('save')
+saveBtn.addEventListener('click', () => {
+    const dataUrl = canvas.toDataURL("image/new.png").replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+   const newTab = window.open('about:blank','image from canvas');
+   newTab.document.write("<img src='" + dataUrl + "' alt='from canvas'/>");
+})
+
+const localStr = document.getElementById("local");
+localStr.addEventListener("click", () => {
+	localStorage.setItem('points', JSON.stringify(points));
+})
+
+const restore = document.getElementById("restore");
+restore.addEventListener("click", () => {
+	points = JSON.parse( localStorage.getItem('points'));
+	redraw();
+})
+
+const time2 = document.getElementById("time1");
+
+time2.addEventListener("click", () => {
+	time = new Date()
+	console.log(time.toLocaleTimeString())
+	context.fillText(time.toLocaleTimeString(), 50, 50);
+})
+
+
+var theInput = document.getElementById("color-picker"); 
+
+	theInput.addEventListener("input", () => {
+	   var theColor = theInput.value; 
+	   options.strokeColor = theColor
+	   console.log( options.strokeColor)   
+
+	}, false);
+	
+	var size = document.getElementById("txt");
+	
+	size.addEventListener("input", () => {
+		options.strokeWidth = size.value;
+		console.log(size.value);
+	});
+	
+	var i = document.getElementById("img");
+	i.addEventListener("input", ()=>{
+		const img = new Image;
+		img.src = i.value;
+		img.onload = () => {
+		   context.drawImage(img, 0, 0);
+		}
+	});
+}
---
+++ pr6/assets/js/script.js
@@ -0,0 +1,72 @@
+﻿function Slide(index, title, background, link ) {
+   this.title = title;
+   this.background = background;
+   this.link = link;
+   this.id = "slide-" + index;
+}
+
+const Slider = {
+   current: 0,
+   slides: [],
+   initSlider: function(slides){
+       let index = 0;
+       for (let slide of slides){
+           this.slides.push(new Slide(index, slide.title, slide.background, slide.link));
+           index++;
+       }
+       this.buildSlider();
+   },
+   buildSlider: function() {
+       let sliderHTML = "";
+	   let counter=0;
+       for(let slide of this.slides) {
+//зверніть увагу на можливість використання ``,яка дозволяє додавати в string змінні ${}
+           sliderHTML +=
+               `<div id='${slide.id}' class='singleSlide'
+           style='background-image:url(${slide.background});'>
+           <div class='slideOverlay'>
+           <h2>${slide.title}</h2>
+           <a class='link' href='${slide.link}' target='_blank'>Open</a></div></div>`;
+		   counter+=1;
+       }
+	   
+       document.getElementById("slider").innerHTML = sliderHTML;
+       document.getElementById("slide-" + this.current).style.left = 0;
+	   sliderHTML = "";
+	   let i = 0;
+	   for(; i < counter; i++) sliderHTML += `<input id='${i}' type="button" value="${i+1}" onclick="b(${i})"></input>`;
+	   document.getElementById("bs").innerHTML  = sliderHTML;
+   },
+   prevSlide: function() {
+       let next;
+       if (this.current === 0 ) {
+           next = this.slides.length - 1;
+       } else {
+           next = this.current - 1;
+       }
+
+       document.getElementById("slide-" + next).style.left = "-100%";
+       document.getElementById("slide-" + this.current).style.left = 0;
+
+       document.getElementById("slide-" + next).setAttribute("class", "singleSlide slideInLeft");
+       document.getElementById("slide-" + this.current).setAttribute("class", "singleSlide slideOutRight");
+
+       this.current = next;
+   },
+   nextSlide: function(){
+       let next;
+       if (this.current === (this.slides.length - 1) ) {
+           next = 0;
+       } else {
+           next = this.current + 1;
+       }
+
+       document.getElementById("slide-" + next).style.left = "100%";
+       document.getElementById("slide-" + this.current).style.left = 0;
+
+       document.getElementById("slide-" + next).setAttribute("class", "singleSlide slideInRight");
+       document.getElementById("slide-" + this.current).setAttribute("class", "singleSlide slideOutLeft");
+
+       this.current = next;
+   }
+}
